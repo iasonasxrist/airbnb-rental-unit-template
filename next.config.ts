90f2b327e6a0cb68a7ab2bf6fb09168fffa3ac6b
iasonasxrist <iasonasxrist@gmail.com>
2025-04-06 21:24:10 +0300

    update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        // You can add port and pathname if needed, but often hostname is enough
+      },
+      // Add other hostnames if you use images from other external sources
+    ],
+  },
 };
 
 export default nextConfig;
